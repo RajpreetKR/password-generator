de767aa486cf0902326d04685de4b87471559913
RajpreetKR <rajpreet@khalsa.com>
2024-01-07 12:58:35 +0000

    added code that will prompt and alert the user for the password length as needed

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # password-generator
 5th Module Challenge for the Front-End Web Development Bootcamp
+
+
+https://discuss.codecademy.com/t/how-to-validate-a-range-of-number-input-in-javascript-and-end-the-loop-with-a-designated-input/616362
--- script.js
+++ script.js
@@ -12,8 +12,22 @@ var upperCasedCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K
 
 // Function to prompt user for password options
 function getPasswordOptions() {
-  
+  let passwordLength = prompt("Please enter your desired password length between the numbers 8 and 128:");
+
+  if (passwordLength === null) {
+    alert("Error! User canceled the password generation.");
+    return; // Exit the function if the input is null
+  }
+
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    alert("Invalid password length. Please enter a number between 8 and 128.");
+    return; // Exit the function if the input is invalid
+  }
+
+  // The input is valid if the user gets to here
+  return passwordLength;
 }
+
 
 // Function for getting a random element from an array
 function getRandom(arr) {
@@ -23,6 +37,7 @@ function getRandom(arr) {
 // Function to generate password with user input
 function generatePassword() {
   console.log("You clicked the button"); 
+  let options = getPasswordOptions();
 
   return "Generated Password will be here in the future" // testing to see if the button works
 }
